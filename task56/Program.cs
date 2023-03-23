84b4151758979e709b69d7529e1c3f6a724975ec
Nick Gir <mbiko@ya.ru>
2023-03-23 18:14:14 +0300

    add changed task56

--- task56/Program.cs
+++ task56/Program.cs
@@ -39,16 +39,44 @@ int[,] GenerateArray(int numberRows, int numberColumns, int numberStart, int num
     return array;
 }
 
-// Получаем дономерный массив суммы строк двумерного массива 
-// 
-int[] GetSumLinesArray(int[,] tableArray)
+// Вывод одномерного массива
+void PrintArray(int[] array)
 {
-    
+    Console.WriteLine($"[{String.Join(",", array)}]");
 }
 
+// Получаем одономерный массив суммы строк двумерного массива. 
+// Индекс элемента одномерного массива = номеру строки двумерного массива
+int[] GetSumLinesArray(int[,] tableArray)
+{
+   int[] sumElementsLinesArray = new int[tableArray.GetLength(0)];
+   int sum = 0;
+   for (int i = 0; i < tableArray.GetLength(0); i++)
+   {
+    for (int j = 0; j < tableArray.GetLength(1); j++)
+    {
+        sum += tableArray[i,j];
+    }
+    sumElementsLinesArray[i] = sum;
+    sum=0;
+   }
+   return sumElementsLinesArray;  
+}
+
+// Получаем минимальный элемент массива и его индекс.
 (int,int) GetMinElement(int[] array)
 {
-
+    int minElement = array[0];
+    int minIndex = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < minElement)
+        {
+            minElement = array[i];
+            minIndex = i;
+        }
+    }
+    return (minElement, minIndex);
 }
 
 int numberRows = Prompt("Введите значение количества строк массива: ");
@@ -56,14 +84,16 @@ int numberColumns = Prompt("Введите значение количества
 int numberStart = Prompt("Введите начальное значение для заполнения массива: ");
 int numberEnd = Prompt("Введите конечное значение для заполнения массива: ");
 
-int[,] matrix = GenerateArray(numberRows, numberColumns, numberStart, numberStart);
+int[,] matrix = GenerateArray(numberRows, numberColumns, numberStart, numberEnd);
 
-Console.WriteLine("Заданный массив: ")
+Console.WriteLine("Заданный массив: ");
 PrintTableArray(matrix);
 
 int[] sumElementsLinesArray = GetSumLinesArray(matrix);
 
+Console.WriteLine("Массив сумм строк заданного двумерного массива: ");
+PrintArray(sumElementsLinesArray);
 
-(int minSum, int indexRow ) minSum = GetMinElement(sumElementsLinesArray);
-Console.WriteLine($"Наименьшая сумма элементов массива = {minSum} находится в строки {indexRow}");
+(int minSum, int indexRow ) = GetMinElement(sumElementsLinesArray);
+Console.WriteLine($"Наименьшая сумма элементов массива = {minSum} находится в строке {indexRow}");
 
